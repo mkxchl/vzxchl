f1469aa2bd9bebacb92087858e3e3d42955e9b9c
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2025-03-25 15:30:36 +0700

    Add files via upload

---
+++ v/mainxx.js
@@ -0,0 +1,398 @@
+/// Button Card Body Active Function ///
+
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+const ImageA = document.querySelector(".ImageA");
+const VanzMedia = document.querySelector(".VanzMedia");
+const VzMusic = document.querySelector(".VzxMsc");
+
+const handleButtonClick = (e) => {
+  const targetSection = e.target.getAttribute("data-section");
+  const section = document.querySelector(targetSection);
+  targetSection !== "#home" ? card.classList.add("is-active") : card.classList.remove("is-active");
+  card.setAttribute("data-state", targetSection);
+  sections.forEach((s) => s.classList.remove("is-active"));
+  buttons.forEach((b) => b.classList.remove("is-active"));
+  klik.play();
+  e.target.classList.add("is-active");
+  section.classList.add("is-active");
+};
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", handleButtonClick);
+});
+
+var SongAudio = new Audio("https://editor-miring-squad.netlify.app/mix/mk-anime-song.mp3");
+SongAudio.loop = false;
+SongAudio.autoplay = false;
+
+function VzxxxLoad() {
+  setTimeout(function () {
+    VzMusic.classList.add("VzUpdate");
+    SongAudio.play();
+  }, 1500);
+}
+
+function OpenImgA() {
+  window.location.replace("https://editor-miring-squad.netlify.app/assets/images/TNG.jpg");
+  klik.play();
+}
+function OpenImgB() {
+  window.location.replace("https://editor-miring-squad.netlify.app/assets/images/tca.jpg");
+  klik.play();
+}
+function OpenMenu() {
+  VanzMedia.classList.add("VanzMediaShow");
+  klik.play();
+  
+}
+function CloseMenu() {
+  VanzMedia.classList.remove("VanzMediaShow");
+  klik.play();
+}
+var klik = new Audio("https://editor-miring-squad.netlify.app/am/mix/klik.mp3");
+klik.loop = false;
+klik.autoplay = false;
+
+/// Dark Mode Function ///
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line");
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", () => {
+  closeBtn.classList.toggle("open");
+});
+
+function playMyAudio() {
+  document.getElementById("myAudio").play();
+}
+
+/// Function Link  ///
+function mkFacebook() {
+  window.location.replace("https://www.facebook.com/chellgnzxz");
+}
+function mkSptf() {
+  window.location.replace("https://open.spotify.com/playlist/1ZaVZkzQxjsSPUSnLDrL27?si=4362b6d2141140a9");
+}
+function mkInstagram() {
+  window.location.replace("https://www.instagram.com/chellgnzxz_");
+}
+function mkWebsite() {
+  window.location.replace("https://mk-links.netlify.app/");
+}
+function mkGit() {
+  window.location.replace("https://github.com/mkxchl");
+}
+function mkSoundcloud() {
+  window.location.replace("https://soundcloud.com/marchellkevandra");
+}
+function MKAudioxxx() {
+  SongAudio.play();
+}
+document.querySelector(".hati").addEventListener("click", function () {
+  confetti();
+  const teks = document.getElementById("teks");
+  const btn = document.querySelector(".tombol");
+  teks.classList.remove("d-none");
+  btn.classList.add("d-none");
+  console.log(teks);
+  console.log(btn);
+});
+
+("use strict");
+
+var onlyOnKonami = false;
+
+function confetti() {
+  // Globals
+  var $window = $(window),
+    random = Math.random,
+    cos = Math.cos,
+    sin = Math.sin,
+    PI = Math.PI,
+    PI2 = PI * 2,
+    timer = undefined,
+    frame = undefined,
+    confetti = [];
+
+  var runFor = 2000;
+  var isRunning = true;
+
+  setTimeout(() => {
+    isRunning = false;
+  }, runFor);
+
+  // Settings
+  var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+    pointer = 0;
+
+  var particles = 150,
+    spread = 20,
+    sizeMin = 5,
+    sizeMax = 12 - sizeMin,
+    eccentricity = 10,
+    deviation = 100,
+    dxThetaMin = -0.1,
+    dxThetaMax = -dxThetaMin - dxThetaMin,
+    dyMin = 0.13,
+    dyMax = 0.18,
+    dThetaMin = 0.4,
+    dThetaMax = 0.7 - dThetaMin;
+
+  var colorThemes = [
+    function () {
+      return color((200 * random()) | 0, (200 * random()) | 0, (200 * random()) | 0);
+    },
+    function () {
+      var red = (200 * random()) | 0;
+      return color(200, red, red);
+    },
+    function () {
+      var red = (200 * random()) | 0;
+      return color(red, 200, red);
+    },
+    function () {
+      var red = (200 * random()) | 0;
+      return color(red, red, 200);
+    },
+    function () {
+      return color(200, 100, (200 * random()) | 0);
+    },
+    function () {
+      return color((200 * random()) | 0, 200, 200);
+    },
+    function () {
+      var red = (256 * random()) | 0;
+      return color(red, red, red);
+    },
+    function () {
+      return colorThemes[random() < 0.5 ? 1 : 2]();
+    },
+    function () {
+      return colorThemes[random() < 0.5 ? 3 : 5]();
+    },
+    function () {
+      return colorThemes[random() < 0.5 ? 2 : 4]();
+    },
+  ];
+
+  function color(r, g, b) {
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
+  // Cosine interpolation
+  function interpolation(a, b, t) {
+    return ((1 - cos(PI * t)) / 2) * (b - a) + a;
+  }
+
+  // Create a 1D Maximal Poisson Disc over [0, 1]
+  var radius = 1 / eccentricity,
+    radius2 = radius + radius;
+
+  function createPoisson() {
+    // domain is the set of points which are still available to pick from
+    // D = union{ [d_i, d_i+1] | i is even }
+    var domain = [radius, 1 - radius],
+      measure = 1 - radius2,
+      spline = [0, 1];
+    while (measure) {
+      var dart = measure * random(),
+        i,
+        l,
+        interval,
+        a,
+        b,
+        c,
+        d;
+
+      // Find where dart lies
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2) {
+        (a = domain[i]), (b = domain[i + 1]), (interval = b - a);
+        if (dart < measure + interval) {
+          spline.push((dart += a - measure));
+          break;
+        }
+        measure += interval;
+      }
+      (c = dart - radius), (d = dart + radius);
+
+      // Update the domain
+      for (i = domain.length - 1; i > 0; i -= 2) {
+        (l = i - 1), (a = domain[l]), (b = domain[i]);
+        // c---d          c---d  Do nothing
+        //   c-----d  c-----d    Move interior
+        //   c--------------d    Delete interval
+        //         c--d          Split interval
+        //       a------b
+        if (a >= c && a < d)
+          if (b > d) domain[l] = d; // Move interior (Left case)
+          else domain.splice(l, 2);
+        // Delete interval
+        else if (a < c && b > c)
+          if (b <= d) domain[i] = c; // Move interior (Right case)
+          else domain.splice(i, 0, c, d); // Split interval
+      }
+
+      // Re-measure the domain
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2) measure += domain[i + 1] - domain[i];
+    }
+
+    return spline.sort();
+  }
+
+  // Create the overarching container
+  var container = document.createElement("div");
+  container.style.position = "fixed";
+  container.style.top = "0";
+  container.style.left = "0";
+  container.style.width = "100%";
+  container.style.height = "0";
+  container.style.overflow = "visible";
+  container.style.zIndex = "9999";
+
+  // Confetto constructor
+  function Confetto(theme) {
+    this.frame = 0;
+    this.outer = document.createElement("div");
+    this.inner = document.createElement("div");
+    this.outer.appendChild(this.inner);
+
+    var outerStyle = this.outer.style,
+      innerStyle = this.inner.style;
+    outerStyle.position = "absolute";
+    outerStyle.width = sizeMin + sizeMax * random() + "px";
+    outerStyle.height = sizeMin + sizeMax * random() + "px";
+    innerStyle.width = "100%";
+    innerStyle.height = "100%";
+    innerStyle.backgroundColor = theme();
+
+    outerStyle.perspective = "50px";
+    outerStyle.transform = "rotate(" + 360 * random() + "deg)";
+    this.axis = "rotate3D(" + cos(360 * random()) + "," + cos(360 * random()) + ",0,";
+    this.theta = 360 * random();
+    this.dTheta = dThetaMin + dThetaMax * random();
+    innerStyle.transform = this.axis + this.theta + "deg)";
+
+    this.x = $window.width() * random();
+    this.y = -deviation;
+    this.dx = sin(dxThetaMin + dxThetaMax * random());
+    this.dy = dyMin + dyMax * random();
+    outerStyle.left = this.x + "px";
+    outerStyle.top = this.y + "px";
+
+    // Create the periodic spline
+    this.splineX = createPoisson();
+    this.splineY = [];
+    for (var i = 1, l = this.splineX.length - 1; i < l; ++i) this.splineY[i] = deviation * random();
+    this.splineY[0] = this.splineY[l] = deviation * random();
+
+    this.update = function (height, delta) {
+      this.frame += delta;
+      this.x += this.dx * delta;
+      this.y += this.dy * delta;
+      this.theta += this.dTheta * delta;
+
+      // Compute spline and convert to polar
+      var phi = (this.frame % 7777) / 7777,
+        i = 0,
+        j = 1;
+      while (phi >= this.splineX[j]) i = j++;
+      var rho = interpolation(this.splineY[i], this.splineY[j], (phi - this.splineX[i]) / (this.splineX[j] - this.splineX[i]));
+      phi *= PI2;
+
+      outerStyle.left = this.x + rho * cos(phi) + "px";
+      outerStyle.top = this.y + rho * sin(phi) + "px";
+      innerStyle.transform = this.axis + this.theta + "deg)";
+      return this.y > height + deviation;
+    };
+  }
+
+  function poof() {
+    if (!frame) {
+      // Append the container
+      document.body.appendChild(container);
+
+      // Add confetti
+
+      var theme = colorThemes[onlyOnKonami ? (colorThemes.length * random()) | 0 : 0],
+        count = 0;
+
+      (function addConfetto() {
+        if (onlyOnKonami && ++count > particles) return (timer = undefined);
+
+        if (isRunning) {
+          var confetto = new Confetto(theme);
+          confetti.push(confetto);
+
+          container.appendChild(confetto.outer);
+          timer = setTimeout(addConfetto, spread * random());
+        }
+      })(0);
+
+      // Start the loop
+      var prev = undefined;
+      requestAnimationFrame(function loop(timestamp) {
+        var delta = prev ? timestamp - prev : 0;
+        prev = timestamp;
+        var height = $window.height();
+
+        for (var i = confetti.length - 1; i >= 0; --i) {
+          if (confetti[i].update(height, delta)) {
+            container.removeChild(confetti[i].outer);
+            confetti.splice(i, 1);
+          }
+        }
+
+        if (timer || confetti.length) return (frame = requestAnimationFrame(loop));
+
+        // Cleanup
+        document.body.removeChild(container);
+        frame = undefined;
+      });
+    }
+  }
+
+  $window.keydown(function (event) {
+    pointer = konami[pointer] === event.which ? pointer + 1 : +(event.which === konami[0]);
+    if (pointer === konami.length) {
+      pointer = 0;
+      poof();
+    }
+  });
+
+  if (!onlyOnKonami) poof();
+}
+
+var textArray = ["Be your self", "Be carefull", "I Love Girl"];
+var currentIndex = 0;
+var currentText = "";
+
+var interval = setInterval(function () {
+  if (currentIndex == textArray.length) {
+    currentIndex = 0;
+  }
+  currentText = textArray[currentIndex];
+  document.getElementById("text-display").innerHTML = currentText;
+  currentIndex++;
+}, 2000);
