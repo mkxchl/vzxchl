6790f0ac1d0816e7c3cd5c6692252d70f6d43ea8
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2025-03-28 00:06:42 +0700

    Update mainxx.js

--- v/mainxx.js
+++ v/mainxx.js
@@ -1,22 +1,57 @@
-const EmsAuthor = document.querySelector(".VxAuthor");
+const EmsAuthor = document.querySelector('.VxAuthor')
 const VxAuthorContent = document.querySelector('.VxAuthorContent')
 const VxAuthorBtnList = document.querySelector('.VxAuthorBtnList')
-const Contx = document.getElementById('Contx');
-const Homex = document.getElementById('Homex');
-const Profile = document.getElementById('Profile');
-const Profxxx = document.getElementById('Profxxx');
-function VzxLoad() {
-    setTimeout(function () {
-      EmsAuthor.classList.add("AuthorShow");
-    }, 5);
-    setTimeout(function () {
-        VxAuthorContent.classList.add("UpdateHeight");
-        Profile.classList.add('BtnXActive')
-      }, 3000);
-  }
+const Contx = document.getElementById('Contx')
+const Homex = document.getElementById('Homex')
+const Profile = document.getElementById('Profile')
+const Profxxx = document.getElementById('Profxxx')
+const playPauseButton = document.getElementById('playPauseButton')
+const NavMusic = document.querySelector(".NavMusic");
+const VxAuthorMeta = document.querySelector('.VxAuthorMeta')
+let isPlaying = false
 
-function OpenCont() {
-  VxAuthorContent.classList.add('Contactxx');
-  Contx.classList.add('BtnXActive');
+function VzxLoad () {
+  setTimeout(function () {
+    EmsAuthor.classList.add('AuthorShow')
+  }, 5)
+  setTimeout(function () {
+    VxAuthorContent.classList.add('UpdateHeight')
+    Profile.classList.add('BtnXActive')
+  }, 3000)
+}
+
+function OpenCont () {
+  VxAuthorContent.classList.add('Contactxx')
+  Contx.classList.add('BtnXActive')
   Profile.classList.remove('BtnXActive')
 }
+function PlayxMusic () {
+  if (isPlaying) {
+    MkXadios.pause()
+    playPauseButton.classList.remove('bx-pause')
+    playPauseButton.classList.add('bx-play')
+    NavMusic.classList.remove('ShowMsc')
+    VxAuthorMeta.classList.remove('NonActive')
+    VxAuthorContent.classList.remove('DelHeight')
+  } else {
+    MkXadios.play()
+    playPauseButton.classList.remove('bx-play')
+    playPauseButton.classList.add('bx-pause')
+    NavMusic.classList.add('ShowMsc')
+    VxAuthorMeta.classList.add('NonActive')
+    VxAuthorContent.classList.add('DelHeight')
+  }
+  isPlaying = !isPlaying;
+}
+function Remove() {
+  VxAuthorContent.classList.remove('DelHeight')
+  NavMusic.classList.remove('ShowMsc')
+    VxAuthorMeta.classList.remove('NonActive')
+}
+var klik = new Audio('https://editor-miring-squad.netlify.app/am/mix/klik.mp3')
+klik.loop = false
+klik.autoplay = false
+
+var MkXadios = new Audio('mix/Sick.mp3')
+MkXadios.loop = false
+MkXadios.autoplay = false
